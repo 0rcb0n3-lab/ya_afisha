4507e46700579a13a64519345317fce886334825
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-09-05 19:18:09 +0300

    added image preview

--- places/admin.py
+++ places/admin.py
@@ -1,13 +1,40 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from places.models import Place, PlaceImage
 
 
-# Register your models here.
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
-    extra = 0
+    extra = 1
+    readonly_fields = ['image_preview']
+    fields = ['image', 'image_preview', 'ordering']
+
+    @admin.display(description='get preview')
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                "<img src='{}' style='max-height: 200px; width: auto;' />,",
+                obj.image.url,
+            )
+        return 'Нет изображения'
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [PlaceImageInline]
+
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    list_display = ['place', 'ordering', 'image']
+    list_editable = ['ordering']
+    readonly_fields = ['image_preview']
+
+    @admin.display(description='Превью')
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-height: 200px; width: auto;" />',
+                obj.image.url,
+            )
+        return 'Нет изображения'
