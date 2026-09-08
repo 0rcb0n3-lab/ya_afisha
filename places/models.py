a362ad09fcdab8544f09bb7e9ba31b0a180b1887
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-09-09 00:41:32 +0300

    moved from models to view

--- places/models.py
+++ places/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.urls import reverse
 
 
 class Place(models.Model):
@@ -15,32 +14,6 @@ class Place(models.Model):
 
     def __str__(self) -> str:
         return self.title
-
-    def as_feature(self) -> dict:
-        return {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [self.lng, self.lat],
-            },
-            "properties": {
-                "title": self.title,
-                "placeId": self.pk,
-                "detailsUrl": reverse("place_detail", kwargs={"pk": self.pk}),
-            },
-        }
-
-    def as_json(self) -> dict:
-        return {
-            "title": self.title,
-            "imgs": [img.image.url for img in self.images.all()],
-            "description_short": self.short_description,
-            "description_long": self.long_description,
-            "coordinates": {
-                "lat": self.lat,
-                "lng": self.lng,
-            },
-        }
 
 
 class PlaceImage(models.Model):
--- places/views.py
+++ places/views.py
@@ -1,7 +1,36 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from places.models import Place
+
+
+def place_to_feature(place):
+    return {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [place.lng, place.lat],
+        },
+        "properties": {
+            "title": place.title,
+            "placeId": place.pk,
+            "detailsUrl": reverse("place_detail", kwargs={"pk": place.pk}),
+        },
+    }
+
+
+def place_to_json(place):
+    return {
+        "title": place.title,
+        "imgs": [img.image.url for img in place.images.all()],
+        "description_short": place.short_description,
+        "description_long": place.long_description,
+        "coordinates": {
+            "lat": place.lat,
+            "lng": place.lng,
+        },
+    }
 
 
 def index(request):
@@ -11,7 +40,7 @@ def index(request):
         {
             "places_geojson": {
                 "type": "FeatureCollection",
-                "features": [place.as_feature() for place in Place.objects.all()],
+                "features": [place_to_feature(place) for place in Place.objects.all()],
             }
         },
     )
@@ -20,6 +49,6 @@ def index(request):
 def place_detail(request, pk):
     place = get_object_or_404(Place.objects.prefetch_related('images'), pk=pk)
     return JsonResponse(
-        place.as_json(),
+        place_to_json(place),
         json_dumps_params={"ensure_ascii": False, "indent": 2},
     )
